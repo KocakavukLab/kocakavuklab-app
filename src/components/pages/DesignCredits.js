224403751fccc7d118081e7ea2bc5d53ed9cff19
Pranav Swaroop Gundla <30660992+BioCoderR@users.noreply.github.com>
2023-09-02 09:58:44 +0200

    new repo adding build

--- src/components/pages/DesignCredits.js
+++ src/components/pages/DesignCredits.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, useRouteLoaderData } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { FaReact, FaFontAwesome, FaThumbsUp, FaHandPointDown } from "react-icons/fa";
 
 
