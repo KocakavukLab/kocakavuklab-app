039b544875b5f9f26ba53f5e2fe02f227c50ccec
Pranav Swaroop Gundla <30660992+BioCoderR@users.noreply.github.com>
2025-12-12 13:04:16 +0100

    newspage details and assets pictures

--- src/components/pages/News.js
+++ src/components/pages/News.js
@@ -1,16 +1,21 @@
-import React from "react";
+
 import { useNavigate } from "react-router-dom";
-import confetti from "canvas-confetti"; // Import confetti
+import confetti from "canvas-confetti";
 import {
   VerticalTimeline,
   VerticalTimelineElement
 } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
 import PageHeader from "../common/PageHeader";
-// Icons
-import { FaMoneyBillWave, FaUserPlus, FaNewspaper } from "react-icons/fa";
-import { motion } from "framer-motion";
 import GoToTopButton from "../common/GoToTopButton";
+import {
+  getSortedNews
+} from "../../data/newsData";
+// Icons
+import { FaMoneyBillWave, FaUserPlus, FaNewspaper, FaAward } from "react-icons/fa";
+import { motion } from "framer-motion";
+import '../../App.css';
+
 const fadeInEffect = {
   hidden: { opacity: 0 },
   visible: { opacity: 1, transition: { duration: 1.2 } }
@@ -28,115 +33,7 @@ const glowEffect = {
     transition: { duration: 2, repeat: 1 }
   }
 };
-// News Data
-const news = [
-    // ESMO Merit Award
-    {
-        title: "ESMO Merit Award",
-        date: "November 2025",
-        description:
-            "<p>Our PhD student Pranav Swaroop Gundla has been awarded the prestigious ESMO Merit Award at the ESMO AI & Digital Oncology Congress 2025. " +
-            "The study highlights how foundation models and attention-based multiple instance learning (MIL) can help decode the molecular landscape of gliomas directly from H&E-stained whole slide images (WSIs) — moving one step closer toward precision pathology.</p>" +
-            "<p>Checkout the poster <a href='https://doi.org/10.1016/j.esmorw.2025.100474' target='_blank'>here</a></p>" +
-            "<p><a href='https://www.linkedin.com/feed/hashtag/esmoai25/' target='_blank'>#ESMOAI25</a> " +
-            "<a href='https://www.linkedin.com/feed/hashtag/esmomeritaward/' target='_blank'>#ESMOMeritAward</a></p>",
-        image: "/news/Merit_Awardees.jpg",
-        category: "grant",
-        link: "/news/esmo-merit-award"
-    },
-    // Marco's UMEA Grant
-    {
-        title: "Junior Clinician Scientist Fellowship by UMEA",
-        date: "November 2025",
-        description:
-            "Marco Tembrink, MD, has successfully secured a Junior Clinician Scientist fellowship granted by the Clinician Scientist Academy of the University Hospital Essen (UMEA) to join the lab and study focusing on genomic characterization of CNS lymphomas.",
-        image: "/news/umea.png",
-        category: "grant",
-        link: "/news/marco-grant"
-    },
-  // Marco, Jiawei and Quan
-  {
-        title: "New members joining the lab",
-        date: "October 2025",
-        description:
-  "Marco Tembrink joins as a UMEA Clinician Scientist, focusing on genomic characterization of CNS lymphomas.\n\n" +
-  "Jiawei Zhou joins as a Visiting Researcher from Jiangsu Cancer Hospital, working on real-world cancer datasets.\n\n" +
-  "Dr. Quan Shi joins as a Postdoctoral Researcher, funded by the DFG, focusing on the development and application of single-cell and spatial transcriptomics technologies in cancer research.",
-        image: "/news/members2.png",
-        category: "new_member",
-        link:"/news/new-member2"
 
-    },
-    // Cihat, Yinchun, Mahsa, Fatma
-    {
-        title: "New members joining the lab",
-        date: "June 2025",
-        description:
-  "Cihat Karadag MD joins as a Clinician Scientist focused on glioma research and the use of clinicogenomic data in neuro-oncology.\n\n" +
-  "Yinchun Su joins as a PhD student, focusing on tumor heterogeneity through multi-omic data integration.\n\n" +
-  "Mahsasadat Nezamabadi, a Master’s student in Applied Computer Science at the University of Duisburg-Essen, joins as a Student Research Assistant.\n\n" +
-  "Fatma Atak, an undergraduate student in Electrical and Electronics Engineering at Marmara University in Istanbul, Turkey, joins the lab as part of the Erasmus exchange program.",
-        image: "/news/members.png",
-        category: "new_member",
-        link:"/news/new-member"
-
-    },
-    // Fabian Ullrich
-  {
-    title: "Junior Clinician Scientist Fellowship by UMEA",
-    date: "April 2024",
-    description:
-      "Fabian Ullrich, MD, has successfully secured a Junior Clinician Scientist fellowship granted by the Clinician Scientist Academy of the University Hospital Essen (UMEA) to join the lab and study aggressive and central nervous system lymphomas." ,
-    image: "/news/umea.png",
-    category: "grant",
-    link: "/news/umea-fellowship-grant"
-  },
-  // Emmy Noether Grant
-  {
-    title: "Emmy Noether Grant by the DFG",
-    date: "May 2025",
-    description:
-      "This is a true milestone for our laboratory as we have secured funding in the prestigious Emmy Noether Program of the German Research Foundation (DFG). The grant with a funding volume over 2M EUR will support our research for the next six years. Stay tuned for postdoc and PhD positions!" ,
-    image: "/news/logo_emmy_noether.jpg",
-    category: "grant",
-    link: "/news/emmy-noether-grant"
-  },
-  // EKFS Fellowship
-  {
-    title: "Memorial Fellowship by the EKFS",
-    date: "February 2024",
-    description:
-      "We are excited to share the news that Emre Kocakavuk, MD, PhD has received the Memorial Fellowship by the Else Kröner-Fresenius-Stiftung (EKFS). This fellowship will support AI-guided analyses in precision oncology for the next two years.",
-    image: "/news/elsekroner.png",
-    category: "grant",
-    link: "/news/memorial-fellowship"
-  },
-  // Lab opening its doors.
-  {
-    title: "Kocakavuk Lab opening its doors",
-    date: "July 2023",
-    description:
-      "We are happy to share the news that the Kocakavuk Lab with a focus on Computational Oncology has opened. We welcome Pranav Swaroop Gundla, MSc as the first PhD student of the lab.",
-    image: "/news/opening-doors.png",
-    category: "new_member",
-    link: "/news/lab-opening"
-  }
-];
-
-// Sort news items in descending order (latest first)
-// Function to parse date strings into actual Date objects for sorting
-const parseDate = (dateString) => {
-  const months = {
-      January: 0, February: 1, March: 2, April: 3, May: 4, June: 5,
-      July: 6, August: 7, September: 8, October: 9, November: 10, December: 11
-  };
-  
-  const [month, year] = dateString.split(" ");
-  return new Date(parseInt(year), months[month], 1); // Correct month index
-};
-
-// Sort news items in descending order (latest first)
-const sortedNews = [...news].sort((a, b) => parseDate(b.date) - parseDate(a.date));
 // Function to return the correct icon based on the category
 const getCategoryIcon = category => {
   switch (category) {
@@ -144,6 +41,8 @@ const getCategoryIcon = category => {
       return <FaMoneyBillWave />;
     case "new_member":
       return <FaUserPlus />;
+    case "award":
+      return <FaAward />;
     default:
       return <FaNewspaper />;
   }
@@ -160,27 +59,30 @@ const launchConfetti = () => {
 
 function News() {
   const navigate = useNavigate();
+  // Sort news in descending order (newest first: 2025 -> 2023)
+  const allNews = getSortedNews();
 
   const handleNewsClick = item => {
     if (item.category === "grant") {
-      launchConfetti(); // Trigger confetti for grant news
+      launchConfetti();
     }
-    navigate(item.link);
+    navigate(`/news/${item.id}`);
   };
 
   return (
-    <div className="min-h-screen p-10 bg-gray-100 flex flex-col items-center">
-      {/* Reusable Page Header */}
+    <div className="min-h-screen p-10 bg-gray-50 flex flex-col items-center">
+      {/* Page Header */}
       <PageHeader
         title="Latest News"
-        subtitle="Stay updated with our latest achievements!"
+        subtitle="Stay updated with our latest achievements and milestones!"
       />
 
+      {/* Timeline */}
       <VerticalTimeline>
-        {sortedNews.map((item, index) =>
+        {allNews.map((item, index) => (
           <VerticalTimelineElement
             key={index}
-            date={item.date}
+            date={item.dateDisplay}
             contentStyle={{
               background: "#fff",
               color: "#333",
@@ -216,24 +118,18 @@ function News() {
                   {item.title}
                 </h3>}
 
-            {/* Image */}
-            <img
-              src={item.image}
-              alt={item.title}
-              className="w-45 h-20 object-fit rounded-lg shadow-md my-4"
-            />
-
             {/* Description */}
             <p className="text-gray-700">
-              {item.description.split("\n\n").map((para, idx) => (
-  <p key={idx} className="text-gray-700 mb-4">
-    {para}
-  </p>
-))}
+              {item.shortDescription.split("\n\n").map((para, idx) => (
+                <p key={idx} className="text-gray-700 mb-4">
+                  {para}
+                </p>
+              ))}
             </p>
           </VerticalTimelineElement>
-        )}
+        ))}
       </VerticalTimeline>
+
       <GoToTopButton />
     </div>
   );
